f20b17d10fee91528214ff75d669c6246fa596f7
0969108 <0969108@hr.nl>
2021-04-30 11:09:52 +0200

    comments toegevoegd

--- Opdrachten/Opdracht 2/setDistanceToOne.py
+++ Opdrachten/Opdracht 2/setDistanceToOne.py
@@ -1,8 +1,8 @@
 import math as mt
-vector = [689,68]
-def setDistanceToOne(vector):
-    factor = mt.sqrt(mt.pow(vector[0],2) + mt.pow(vector[1],2))
-    vector[0] /= factor
+vector = [689,68] # test vector
+def normalize(vector):
+    factor = mt.sqrt(mt.pow(vector[0],2) + mt.pow(vector[1],2)) #sqaures the two values and take the sqrt from items
+    vector[0] /= factor # take the quotient to normalize the vector
     vector[1] /= factor
-    return vector
-print(setDistanceToOne(vector))
+    return vector # return the normalized vector
+print(normalize(vector))
