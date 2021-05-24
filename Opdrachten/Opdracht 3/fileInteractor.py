b859e9d737be7172bd4f523b6b97ed5a3c71fb24
Jayden Wev <jaydenwev@gmail.com>
2021-05-24 13:23:06 +0200

    Updated fileInteractor

--- Opdrachten/Opdracht 3/fileInteractor.py
+++ Opdrachten/Opdracht 3/fileInteractor.py
@@ -1,20 +1,17 @@
-# Jayden
-import numpy as np
-from csv import writer as wr
+import csv
 
-class fileInteractor:
-    def storeInFile(self, fileName, values):
-        np.savetxt(fileName, values, delimiter=",")
+def writeToFile(path, data):
+    f = open(path, 'w')
+    for block in data:
+        for t in block:
+            line = '-'.join(str(x) for x in t)
+            f.write(line + '\n')
+    f.close()
 
-    def storeNewElement(self, fileName, element):
-        # Open file in append mode
-        with open(fileName, 'a+', newline='') as write_obj:
-            csv_writer = wr(write_obj) # Create a writer object from csv module
-            csv_writer.writerow(element)
-            write_obj.close()
-
-    def readFromFile(self, fileName):
-        return np.genfromtxt(fileName, delimiter=',')
-
-    def readFromFile(self, fileName):
-        return np.genfromtxt(fileName, delimiter=',')
+def readFromFile(path):
+    dataFile = open(path, 'r')
+    dataReader = csv.reader(dataFile, delimiter = '-')
+    data = []
+    for row in dataReader:
+        data.append(row)
+    return data
