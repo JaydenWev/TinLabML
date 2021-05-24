b68bd7d77582319390954dc02f49ed5e90caf815
Jayden Wev <jaydenwev@gmail.com>
2021-05-24 13:24:11 +0200

    Saving to and reading from files

--- Opdrachten/Opdracht 3/main.py
+++ Opdrachten/Opdracht 3/main.py
@@ -40,20 +40,34 @@ class MusicGenerator:
     Zet de generation/blocken om naar een muziek file
     '''
 
-note_A4 = Note('a', 4)
-note_A8 = Note('a', 8)
-note_B4 = Note('b', 4)
-note_B8 = Note('b', 8)
-note_C4 = Note('c', 4)
-note_C8 = Note('c', 8)
-note_D4 = Note('d', 4)
-note_D8 = Note('d', 8)
-note_E4 = Note('e', 4)
-note_E8 = Note('e', 8)
 
-aNotes = [note_A4, note_A8]
+#where the music is stored
+pathToFile = 'Opdrachten/Opdracht 3/data.csv'
+block = (
+    (
+        ('e', 8), ('f#', 8),
+        ('g*', 4), ('f#', 8), ('e', 8), ('d#*', 4), ('e', 8), ('f#', 8),
+        ('b3*', 4), ('c#', 8), ('d#', 8), ('e*', 4), ('d', 8), ('c', 8),
+        ('b3*', 4), ('a3', 8), ('g3', 8), ('f#3*', 4), ('g3', 8), ('a3', 8)
+    ),
+    (
+        ('e', 4), ('f#', 4),
+    )
+),
 
-block_1 = Block(aNotes)
+smallBlock = (
+    (
+        ('e', 8), ('f#', 8),
+    ),
+    (
+        ('e', 4), ('f#', 4),
+    )
+),
 
-print(block_1.notes[0].getValues())
-print(block_1.notes[1].getValues())
+
+fi.writeToFile(pathToFile, smallBlock)
+data = fi.readFromFile(pathToFile)
+
+print(data[0][0])
+print('\n')
+print(data)
