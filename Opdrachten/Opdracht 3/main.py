fb7470aee805ecc9db626a1c13db1ec07085c4ea
Keanu Linden <0967019@hr.nl>
2021-05-28 12:01:10 +0200

    added many things

--- Opdrachten/Opdracht 3/main.py
+++ Opdrachten/Opdracht 3/main.py
@@ -1,39 +1,13 @@
 import muser as ms
-import fileInteractor as fi
+
+import musicBlocks as mbLib
 # ¿¿Optie voor afspeelsnelheid/BPM erbij zetten??
 
 #where the music is stored
 csvDir = 'Opdrachten/Opdracht 3/csv/'
 songDir = 'Opdrachten/Opdracht 3/music/'
 
-firstBlock = [
-    [
-        ('f', 4),
-        ('g', 4),
-        ('f', 4),
-        ('g', 4), 
-    ],
-    [
-        ('a', 16),('b', 16),('c', 16),('d', 16),
-        ('a', 16),('b', 16),('c', 16),('d', 16),
-        ('a', 16),('b', 16),('c', 16),('d', 16),
-        ('a', 16),('b', 16),('c', 16),('d', 16),
-    ],
-]
-secondBlock = [
-    [
-        ('e', 4),
-        ('d', 4),
-        ('b', 4),
-        ('a', 4),  
-    ],
-    [
-        ('c2', 16),('c2', 16),('c2', 16),('c2', 16),
-        ('c2', 12),('c2', 12),('c2', 12),
-        ('c2', 8),('c2', 8),
-        ('c2', 4),
-    ],
-]
+
 '''
 # Save to file
 csvName = 'data.csv'
@@ -82,9 +56,7 @@ class UserInputHandeler:
 class BlockController:
     baseBlocks = []
     currentBlocks = [[],[]]
-    def setCurrentBlock(self, blockToSet):
-        self.currentBlocks = blockToSet
-        #print("current: ",self.currentBlocks)
+    blockIDs = []
     def readBaseBlocks(self): # Read the base/starter blocks
         pass
 
@@ -94,10 +66,12 @@ class BlockController:
     def updateBlockScore(self, adjustment): # Updates the score of currently used blocks
         pass
     
-    def addBlockToSong (self, returnedBlock):
-        for x, row in enumerate(returnedBlock):
-            for y, column in enumerate(returnedBlock[x]):
-                self.currentBlocks[x].append(returnedBlock[x][y])
+    def addBlockToSong (self, blockID):
+        for x, row in enumerate(mbLib.blocks[blockID]):
+            for y, column in enumerate(mbLib.blocks[blockID][x]):
+                print("pre append")
+                self.currentBlocks[x].append(mbLib.blocks[blockID][x][y])
+                print("post append")
         #print ("current after adding:",self.currentBlocks)
 
 
@@ -105,12 +79,16 @@ sc = SongCreater()
 user = UserInputHandeler()
 bc = BlockController()
 muser = ms.Muser ()
+print(mbLib.blocks[0][0])
 
 
-bc.setCurrentBlock(firstBlock) #overwrites current block with parameter
-bc.addBlockToSong(secondBlock) #adds block to current block located in bc
+
+#bc.setCurrentBlock(mbLib.blocks[0]) #overwrites current block with parameter
+bc.addBlockToSong(2) #adds block to current block located in bc
+bc.addBlockToSong(1)
+bc.addBlockToSong(0)
 sc.generateSong(bc.currentBlocks,"song1.wav")
-
+print("currentBlocks: ", bc.currentBlocks)
 
 
 
---
+++ Opdrachten/Opdracht 3/music/bach.py
@@ -0,0 +1,15 @@
+import muser as ms
+
+bach = (
+    (
+        ('e*', 4),
+
+    ),
+    (
+        ('g2', 8),
+
+    )
+)
+
+muser = ms.Muser ()
+muser.generate (bach)
