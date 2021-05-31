d89add5035862ab693ea0d4e0413d5aa9cc1baea
Jayden Wev <jaydenwev@gmail.com>
2021-05-31 22:16:09 +0200

    Fixed interactor json

--- Opdrachten/Opdracht 3/FileInteractorJSON.py
+++ Opdrachten/Opdracht 3/FileInteractorJSON.py
@@ -7,10 +7,4 @@ def writeToFile(path, data):
 def readFromFile(path):
     with open(path) as File:
         score = json.load(File)
-        print(score)
-
-scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
-#writeToFile('data.json', scores)
-
-readFromFile('data.json')
+        return score
