22607549b759f8bdc529393a3c3755b35b267bf4
boppert <80949541+boppert@users.noreply.github.com>
2021-05-31 21:21:46 +0200

    scores in json

--- Opdrachten/Opdracht 3/FileInteractorJSON.py
+++ Opdrachten/Opdracht 3/FileInteractorJSON.py
@@ -1,11 +1,16 @@
 import json
 
 def writeToFile(path, data):
-    with open('data.json', 'w') as outfile:
+    with open(path, 'w') as outfile:
         json.dump(data, outfile)
 
-def readFromFile(path, variable):
-    with open('data.json') as File:
-        bach2 = json.load(File)
-        for p in bach2:
-            return p[variable]
+def readFromFile(path):
+    with open(path) as File:
+        score = json.load(File)
+        print(score)
+
+scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+#writeToFile('data.json', scores)
+
+readFromFile('data.json')
