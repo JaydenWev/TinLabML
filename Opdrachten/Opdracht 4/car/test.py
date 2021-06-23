55bc230c8c125b4126e99bb6e976997c8eaa4a24
Jayden Wev <jaydenwev@gmail.com>
2021-06-23 16:03:23 +0200

    Test

---
+++ Opdrachten/Opdracht 4/car/test.py
@@ -0,0 +1,4 @@
+import FileInteractorJSON as fi
+
+fi.writeToFile('path.path', 'D:\Documents\School\Tinlab\Tinlab ML\TinLabML\Opdrachten\Opdracht 4\car\simpylc')
+print(fi.readFromFile('path.path'))
